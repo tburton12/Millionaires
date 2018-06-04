bdcea25a4d2ec75f9d6630af3377825d4fafc364
tburton12 <p.wrzesien@o2.pl>
2018-06-04 11:21:32 +0100

    Created class Questions Added StartingScreenActivity class Added functionality to START button

---
+++ app/src/main/java/com/example/tburton/millionaires/Question.java
@@ -0,0 +1,10 @@
+package com.example.tburton.millionaires;
+
+public class Question {
+    private String question;
+    private String option1;
+    private String option2;
+    private String option3;
+    private int answerNr;
+
+}
--- app/src/main/java/com/example/tburton/millionaires/StartingScreenActivity.java
+++ app/src/main/java/com/example/tburton/millionaires/StartingScreenActivity.java
@@ -2,6 +2,9 @@ package com.example.tburton.millionaires;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class StartingScreenActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class StartingScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
+
+        Button buttonStartGame = findViewById(R.id.button_start_game);
+        buttonStartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
+
+    }
+
+     private void startGame() {
+        Intent intent = new Intent(StartingScreenActivity.this, GameActivity.class);
+        startActivity(intent);
     }
 }
