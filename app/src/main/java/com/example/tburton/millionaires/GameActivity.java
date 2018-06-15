cfe04daf000fc40fdfc90205097c8a09cfe96997
tburton12 <p.wrzesien@o2.pl>
2018-06-15 12:19:05 +0100

    Done: Finishing game activity Edited game layout

--- app/src/main/java/com/example/tburton/millionaires/GameActivity.java
+++ app/src/main/java/com/example/tburton/millionaires/GameActivity.java
@@ -4,7 +4,6 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -13,7 +12,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
-import java.util.Objects;
 
 public class GameActivity extends AppCompatActivity {
     private TextView textViewQuestion;
@@ -155,8 +153,7 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
-    // TODO: Do finishGame function
     private void finishGame(){
-
+        finish();
     }
 }
